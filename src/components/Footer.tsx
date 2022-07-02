f9149a4c139cfd063918865a08cd901d93b2e584
Kyle Conrad <kwconrad@protonmail.com>
2022-07-02 16:58:47 -0700

    Fix footer contrast

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="mx-auto">
-      <span>© {new Date().getFullYear()} Kyle Conrad</span>
+    <div className="mx-auto py-2">
+      <span className="text-white text-sm">
+        © {new Date().getFullYear()} Kyle Conrad
+      </span>
     </div>
   );
 }

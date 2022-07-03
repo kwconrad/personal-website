c1ce168bbb420eaa91feb3655b18237f4729026b
Kyle Conrad <kwconrad@protonmail.com>
2022-07-02 17:17:32 -0700

    Update button color and orientation for homepage; update font-sizing for hero on desktop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,39 +12,33 @@ const Home = () => {
         <div className="px-4 md:px-6 lg:px-8 pt-6">
           <div className="pb-16 flex flex-col gap-6">
             <div className="flex flex-col gap-3">
-              <h1 className="text-4xl font-bold text-white">
-                I craft delightful product experiences
+              <h1 className="text-4xl md:text-5xl font-bold text-white">
+                I craft product experiences
               </h1>
               <p className="text-white">
                 Finding creative solutions to tricky problems has always been my
-                joy in life. I try to bring a sense of joy and fun into every
-                problem I solve for the organizations and individuals that I
-                work with.
+                joy in life. I try to bring that joy and feeling of fun into
+                every problem I solve, for the organizations and individuals
+                that I work with.
               </p>
             </div>
             <div className="flex items-center gap-4">
-              <div className="relative">
-                <div className="absolute inset-0 bg-pink-600 rounded-full transition-all duration-150"></div>
-                <a
-                  className="relative px-6 py-3 bg-gray-1100 hover:bg-gray-900 text-sm text-white font-bold rounded-full transition-colors duration-150 ease-in-out"
-                  href="/cv-2022.pdf"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Download Resumé
-                </a>
-              </div>
-              <div className="relative">
-                <div className="absolute inset-0 bg-pink-600 rounded-full transition-all duration-150"></div>
-                <a
-                  className="relative px-6 py-3 bg-gray-1100 hover:bg-gray-900 text-sm text-white font-bold rounded-full transition-colors duration-150 ease-in-out"
-                  href="https://dribbble.com/easternsun"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  See my work
-                </a>
-              </div>
+              <a
+                className="relative px-6 py-3 bg-white hover:bg-gray-400 text-sm text-gray-1200 font-bold rounded-full transition-colors duration-150 ease-in-out"
+                href="https://dribbble.com/easternsun"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                See my portfolio
+              </a>
+              <a
+                className="relative px-6 py-3 bg-gray-1000 hover:bg-gray-800 text-sm text-white font-bold rounded-full transition-colors duration-150 ease-in-out"
+                href="/cv-2022.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download Resumé
+              </a>
             </div>
           </div>
         </div>

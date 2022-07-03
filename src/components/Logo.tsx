00465ad6ba4bd89b882946f2649a18486e524c6a
Kyle Conrad <kwconrad@protonmail.com>
2022-07-02 17:00:48 -0700

    Add new logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,18 @@
+import { IconLogo } from "icons";
+import { routes } from "lib";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Logo() {
+  const router = useRouter();
+
   return (
-    <div className="flex items-center gap-3 rounded-full">
-      <div className="w-12 h-12 bg-brand rounded-full"></div>
+    <button
+      className="flex items-center gap-3 rounded-full"
+      onClick={() => router.push(routes.home)}
+    >
+      <IconLogo className="w-12 h-12 text-white rounded-full" />
       <span className="text-base text-white font-bold">Kyle Conrad</span>
-    </div>
+    </button>
   );
 }
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as IconGitPullRequest } from "./git-pull-request.svg";
 export { default as IconClose } from "./close.svg";
 export { default as IconMenu } from "./menu.svg";
+export { default as IconLogo } from "./logo.svg";

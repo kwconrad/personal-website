693dfbaaf43b744c4295bc6c38a67570d39cdabb
Kyle Conrad <conradwebdev@gmail.com>
2024-07-10 08:35:11 +0100

    improv - upgrade next.js and react

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,27 @@
 export default function Home() {
   return (
-    <main className="h-full min-h-screen w-full bg-cream">
-      <div className="px-4 mx-auto w-full h-20 max-w-3xl">
+    <main className="h-full min-h-screen w-full bg-black">
+      <div className="px-4 mx-auto w-full h-20 max-w-5xl">
         <div className="h-20 w-full flex items-center">
           <div className="flex flex-col">
-            <span className="text-black font-medium font-neue-haas-unica leading-tight tracking-tighter">
+            <span className="text-cream uppercase text-sm font-semibold font-work-sans leading-tight tracking-normal">
               Kyle Conrad
-              <span className="block text-black/75 font-normal font-neue-haas-unica">
-                UX Engineer
+              <span className="block text-cream/75 font-normal font-work-sans">
+                Frontend Engineer
               </span>
             </span>
           </div>
         </div>
       </div>
-      <div className="px-4 mx-auto max-w-3xl h-full flex flex-col justify-center pt-16 gap-6">
+      <div className="px-4 mx-auto max-w-4xl h-full flex flex-col justify-center pt-8 gap-6">
+        <h2 className="text-cream text-2xl font-archivo tracking-tight">
+          👋 Hey, I&apos;m Kyle
+        </h2>
         <div className="flex flex-col">
-          <h1 className="text-black text-7xl font-black font-neue-haas-grotesk-display leading-none tracking-tight">
-            Kyle Conrad,
-          </h1>
-          <h1 className="text-black text-7xl font-black font-neue-haas-grotesk-display leading-none tracking-tight">
-            UX Engineer
+          <h1 className="text-cream text-5xl font-extrabold font-archivo leading-tight tracking-tight">
+            I&apos;m here to help you build awesome user experiences
           </h1>
         </div>
-        <h2 className="text-black text-2xl font-neue-haas-unica">
-          👋 Hi, I&apos;m Kyle, a UX engineer based in Portugal. Reach out to me
-          if you want to work with someone who is just as comfortable designing
-          UIs in Figma as coding frontends in React and Next.js.
-        </h2>
       </div>
     </main>
   );

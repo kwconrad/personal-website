1c50a285b46c84702aa7d60ae8416c6d45cc33e0
Kyle Conrad <conradwebdev@gmail.com>
2024-07-13 07:13:48 +0100

    improv - add footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const LINKEDIN_LINK = "https://www.linkedin.com/in/kylewconrad/";
 
 export default function Home() {
   return (
-    <main className="h-full min-h-screen w-full bg-black flex flex-col">
+    <main className="h-full min-h-screen w-full items-center bg-black flex flex-col">
       <div className="px-4 mx-auto w-full h-20 max-w-4xl">
         <div className="h-20 w-full flex items-center justify-between">
           <div className="flex gap-2 items-center">
@@ -50,43 +50,51 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="select-none touch-none px-4 mx-auto max-w-4xl h-full flex flex-col justify-center pt-16 gap-4 md:gap-6">
-        <h1 className="text-cream text-4xl md:text-6xl font-bold font-archivo leading-[1.125] tracking-tight">
-          Experienced frontend engineer, focused on creating exceptional user
-          experiences
-        </h1>
-        <h2 className="text-cream text-xl font-extralight font-public-sans tracking-tight">
-          👋 Hey, I&apos;m Kyle. I&apos;m passionate about crafting user
-          experiences that are both visually stunning and highly functional.
-          Looking for help with your next project?{" "}
-          <a
-            className="font-semibold underline"
-            href="mailto:kyle@kyleconrad.me"
-          >
-            Reach out!
-          </a>
-        </h2>
-        <div className="flex md:hidden gap-4 items-center">
-          <a
-            href={GITHUB_LINK}
-            target="_blank"
-            className="px-3 py-2 flex-grow-0 bg-cream/20 hover:bg-cream/30 hover:border-cream border border-cream/60 rounded-4 flex items-center gap-1"
-          >
-            <GithubLogo className="w-4 h-4 text-cream"></GithubLogo>
-            <span className="text-sm uppercase font-medium font-roboto-mono text-cream">
-              Github
-            </span>
-          </a>
-          <a
-            href={LINKEDIN_LINK}
-            target="_blank"
-            className="px-3 py-2 flex-grow-0 bg-cream/20 hover:bg-cream/30 hover:border-cream border border-cream/60 rounded-4 flex items-center gap-1"
-          >
-            <LinkedInLogo className="w-4 h-4 text-cream"></LinkedInLogo>
-            <span className="text-sm uppercase font-medium font-roboto-mono text-cream">
-              LinkedIn
-            </span>
-          </a>
+      <div className="select-none touch-none px-4 max-w-4xl flex flex-col justify-between flex-grow items-center pt-16">
+        <div className="flex flex-col gap-4 md:gap-6">
+          <h1 className="text-cream text-4xl md:text-6xl font-bold font-archivo leading-[1.125] tracking-tight">
+            Experienced frontend engineer, focused on creating exceptional user
+            experiences
+          </h1>
+          <h2 className="text-cream text-xl font-extralight font-public-sans tracking-tight">
+            👋 Hey, I&apos;m Kyle. I&apos;m passionate about crafting user
+            experiences that are both visually stunning and highly functional.
+            Looking for help with your next project?{" "}
+            <a
+              className="font-semibold underline"
+              href="mailto:kyle@kyleconrad.me"
+            >
+              Reach out!
+            </a>
+          </h2>
+          <div className="flex md:hidden gap-4 items-center">
+            <a
+              href={GITHUB_LINK}
+              target="_blank"
+              className="px-3 py-2 flex-grow-0 bg-cream/20 hover:bg-cream/30 hover:border-cream border border-cream/60 rounded-4 flex items-center gap-1"
+            >
+              <GithubLogo className="w-4 h-4 text-cream"></GithubLogo>
+              <span className="text-sm uppercase font-medium font-roboto-mono text-cream">
+                Github
+              </span>
+            </a>
+            <a
+              href={LINKEDIN_LINK}
+              target="_blank"
+              className="px-3 py-2 flex-grow-0 bg-cream/20 hover:bg-cream/30 hover:border-cream border border-cream/60 rounded-4 flex items-center gap-1"
+            >
+              <LinkedInLogo className="w-4 h-4 text-cream"></LinkedInLogo>
+              <span className="text-sm uppercase font-medium font-roboto-mono text-cream">
+                LinkedIn
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="h-20 w-full flex justify-center items-center">
+          <span className="text-cream font-public-sans text-sm">
+            © {new Date().getFullYear()}, Kyle Conrad. Built with TypeScript,
+            Next.js and TailwindCSS
+          </span>
         </div>
       </div>
     </main>

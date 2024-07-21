aa4b1319731c0ed55ad551013d75e3b10441ddab
Kyle Conrad <conradwebdev@gmail.com>
2024-07-21 16:36:13 +0100

    fix - update headline

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ export default function Home() {
       <div className="select-none touch-none max-w-4xl flex flex-col justify-between flex-grow items-center pt-16">
         <div className="flex flex-col gap-4 md:gap-6">
           <h1 className="text-cream text-4xl md:text-6xl font-bold font-archivo leading-[1.125] tracking-tight">
-            Experienced Design Engineer crafting design systems and user
-            interactions
+            Experienced Design Engineer crafting systems and user interactions
           </h1>
           <h2 className="text-cream text-xl font-extralight font-public-sans tracking-tight">
             👋 Hey, I&apos;m Kyle. I&apos;m passionate about design systems,

b6bc32dec1eb18f597e287f6c1a172ba7c6e2a8c
Kyle Conrad <conradwebdev@gmail.com>
2024-10-22 07:34:40 +0100

    fix date picker interaction

--- components/date-input.tsx
+++ components/date-input.tsx
@@ -3,16 +3,11 @@ import { useState } from "react";
 import { DatePicker } from "@/components";
 
 export default function DateInput() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
-
   return (
     <div className="flex flex-col gap-2">
       <div className="flex flex-col gap-1">
         <span className="text-neutral-400">Date</span>
-        <DatePicker
-          selectedDate={selectedDate}
-          onSelectDate={(date: Date | null) => setSelectedDate(date)}
-        />
+        <DatePicker />
       </div>
     </div>
   );
--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -1,20 +1,23 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "@/styles/globals.css";
 
-type Props = {
-  selectedDate: Date | null;
-  onSelectDate: (date: Date | null) => void;
-};
+export default function DatePicker() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const selectDate = (date: Date | null) => {
+    console.info("date", date);
 
-export default function DatePicker({ selectedDate, onSelectDate }: Props) {
+    setSelectedDate(date);
+  };
+
   return (
     <ReactDatePicker
       className="w-full rounded-md border border-neutral-700 bg-neutral-800 p-2 text-white outline-none placeholder:text-neutral-500 focus:border-white"
+      adjustDateOnChange
       selected={selectedDate}
-      onChange={onSelectDate}
+      onChange={selectDate}
       dateFormat="yyyy/MM/dd"
     />
   );

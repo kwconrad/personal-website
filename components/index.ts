8f20e5e163ac662d340aae669b54c5c19d580328
Kyle Conrad <kwconrad@protonmail.com>
2025-01-10 10:32:04 +0000

    remove outdated components

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,2 @@
 export { default as Header } from "./header";
 export { default as Footer } from "./footer";
-export { default as Select } from "./select";
-export { default as DatePicker } from "./date-picker";
-export { default as TabTransition } from "./tab-transition";
-export { default as ColorPicker } from "./color-picker";
-export { default as DateInput } from "./date-input";
